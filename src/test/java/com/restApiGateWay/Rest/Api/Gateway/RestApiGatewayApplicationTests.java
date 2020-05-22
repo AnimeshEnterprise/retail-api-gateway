2c740a800d49000f64dd73499ef4069e23a28e75
animeshp <animeshp@INLT957.sapiens.int>
2020-05-22 16:47:28 +0530

    Initial Commit

---
+++ src/test/java/com/restApiGateWay/Rest/Api/Gateway/RestApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restApiGateWay.Rest.Api.Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
